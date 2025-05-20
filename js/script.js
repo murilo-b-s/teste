c5138c1ba1f374575c37dc588e738993c61ab711
Murilo Brum Sena <murilobrumsena08@gmail.com>
2025-05-20 19:34:14 -0300

    ghjk.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById('btn').
+addEventListener('click', function() {
+    this.classList.toggle('open');
+
+    document.getElementById('menu-responsivo').classList.toggle('aberto');
+});
+
