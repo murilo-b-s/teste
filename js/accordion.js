d386ce008ede642fcb6fd178815b0b00ac2d1ec1
Murilo Brum Sena <murilobrumsena08@gmail.com>
2025-05-20 23:29:40 -0300

    fghjklç

--- js/accordion.js
+++ js/accordion.js
@@ -1,45 +1,23 @@
-// var acc = document.getElementsByClassName("accordion");
-// var i;
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     var panel = this.nextElementSibling;
-//     if (panel.style.maxHeight) {
-//       panel.style.maxHeight = null;
-//     } else {
-//       panel.style.maxHeight = panel.scrollHeight + "px";
-//     } 
-//   });
-// }
-
-// document.querySelectorAll('.accordion').forEach(button => {
-//   button.addEventListener('click', function () {
-//       const icon = this.querySelector('svg'); 
-//       icon.classList.toggle('rotate');
-//   });
-// });
-
-
-const accordions = document.querySelectorAll(".accordion");
-
-accordions.forEach((accordion) => {
-  accordion.addEventListener("click", function () {
-    // Alterna classe do botão
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
-
-    // Alterna o painel
-    const panel = this.nextElementSibling;
+    var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
-    }
+    } 
+  });
+}
 
-    // Alterna rotação do ícone (caso tenha um <svg>)
-    const icon = this.querySelector("svg");
-    if (icon) {
-      icon.classList.toggle("rotate");
-    }
-  });
+document.querySelectorAll('.accordion').forEach(button => {
+  button.addEventListener('click', function () {
+      const icon = this.querySelector('svg'); 
+      icon.classList.toggle('rotate');
+  });
 });
+
+
